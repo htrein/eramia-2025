1d60f862dd2350d9083e0d193540d0a72598844b
htrein <trein.lor@gmail.com>
2025-11-12 12:02:32 -0300

    end

---
+++ service.py
@@ -0,0 +1,18 @@
+from features import smiles_to_fingerprints
+from bentoml.io import JSON, NumpyNdarray
+from pydantic import BaseModel
+import numpy as numpy
+import bentoml
+
+class Molecule(BaseModel):
+    smiles:str = ''
+
+bbb_clf_runner = bentoml.sklearn.get("bbb-model:latest").to_runner()
+svc = bentoml.Service("bbb-model", runners=[bbb_clf_runner])
+input_spec = JSON(pydantic_model=Molecule)
+
+@svc.api(input=input_spec, output=NumpyNdarray())
+def classify(molecule: Molecule) -> np.ndarray:
+    features = smiles_to_fingerprints(molecule.smiles)
+    result = bbb_clf_runner.predict.run(np.array([features]))
+    return result
